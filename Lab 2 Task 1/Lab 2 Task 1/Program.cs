31baf4ba7e603c8eed87f93b1e6e17c76bd6f8c7
SoproniukAnna <124136300+SoproniukAnna@users.noreply.github.com>
2024-06-24 13:55:43 +0300

    Lab 2

--- Lab 2 Task 1/Lab 2 Task 1/Program.cs
+++ Lab 2 Task 1/Lab 2 Task 1/Program.cs
@@ -1,142 +1,166 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
+﻿#include <iostream>
+#include <vector>
+#include <thread>
+#include <mutex>
+#include <chrono>
+#include <cstdlib>
 
-namespace VehicleSimulation
+const int Width = 100;
+const int Height = 25;
+const int NumberOfCars = 12;
+const int NumberOfMoto = 8;
+const int Interval = 200; // in milliseconds
+const int Speed = 1;
+
+std::mutex consoleLock;
+
+void setCursorPosition(int x, int y)
 {
-    class Program
+    std::cout << "\033[" << y + 1 << ";" << x + 1 << "H";
+}
+
+void drawBorder()
+{
+    std::cout << "\033[34m"; // Set console color to blue
+
+    for (int i = 0; i <= Width; ++i)
     {
-        const int Width = 100;
-        const int Height = 25;
-        const int NumberOfCars = 12;
-        const int NumberOfMoto = 8;
-        const int Interval = 200; // in milliseconds
-        const int Speed = 1;
-
-        static object consoleLock = new object();
-        static Random rand = new Random();
-
-        static void Main(string[] args)
-        {
-            Console.CursorVisible = false;
-            DrawBorder();
-            List<Car> cars = new List<Car>();
-            List<Motorcycle> motorcycles = new List<Motorcycle>();
-
-            // Initialize cars at random positions
-            for (int i = 0; i < NumberOfCars; ++i)
-            {
-                cars.Add(new Car { x = rand.Next(Width), y = rand.Next(Height) });
-                SetCursorPosition(cars[i].x, cars[i].y);
-                Console.Write("C");
-            }
-
-            // Initialize motorcycles at random positions
-            for (int i = 0; i < NumberOfMoto; ++i)
-            {
-                motorcycles.Add(new Motorcycle { x = rand.Next(Width), y = rand.Next(Height) });
-                SetCursorPosition(motorcycles[i].x, motorcycles[i].y);
-                Console.Write("M");
-            }
-
-            List<Task> tasks = new List<Task>();
-
-            // Start tasks for cars
-            foreach (var car in cars)
-            {
-                tasks.Add(Task.Run(() => MoveCar(car)));
-            }
-
-            // Start tasks for motorcycles
-            foreach (var motorcycle in motorcycles)
-            {
-                tasks.Add(Task.Run(() => MoveMotorcycle(motorcycle)));
-            }
-
-            Task.WaitAll(tasks.ToArray());
-        }
-
-        static void MoveCar(Car car)
-        {
-            while (true)
-            {
-                Thread.Sleep(Interval);
-                int direction = rand.Next(2); // Cars move only along X axis
-
-                lock (consoleLock)
-                {
-                    SetCursorPosition(car.x, car.y);
-                    Console.Write(" ");
-
-                    car.x = direction switch
-                    {
-                        0 => Math.Min(car.x + Speed, Width - 1), // Move right
-                        1 => Math.Max(car.x - Speed, 0), // Move left
-                        _ => car.x,
-                    };
-
-                    SetCursorPosition(car.x, car.y);
-                    Console.Write("C");
-                }
-            }
-        }
-
-        static void MoveMotorcycle(Motorcycle motorcycle)
-        {
-            while (true)
-            {
-                Thread.Sleep(100); // Motorcycles move more frequently
-
-                lock (consoleLock)
-                {
-                    SetCursorPosition(motorcycle.x, motorcycle.y);
-                    Console.Write(" ");
-
-                    motorcycle.y = rand.Next(2) switch
-                    {
-                        0 => Math.Min(motorcycle.y + Speed, Height - 1), // Move down
-                        1 => Math.Max(motorcycle.y - Speed, 0), // Move up
-                        _ => motorcycle.y,
-                    };
-
-                    SetCursorPosition(motorcycle.x, motorcycle.y);
-                    Console.Write("M");
-                }
-            }
-        }
-
-        static void DrawBorder()
-        {
-            Console.ForegroundColor = ConsoleColor.Green;
-
-            for (int i = 0; i <= Width; ++i)
-            {
-                SetCursorPosition(i, Height);
-                Console.Write("*");
-            }
-            for (int i = 0; i <= Height; ++i)
-            {
-                SetCursorPosition(Width, i);
-                Console.Write("*");
-            }
-
-            Console.ResetColor();
-        }
-
-        static void SetCursorPosition(int x, int y)
-        {
-            Console.SetCursorPosition(x, y);
-        }
+        setCursorPosition(i, Height);
+        std::cout << "*";
+    }
+    for (int i = 0; i <= Height; ++i)
+    {
+        setCursorPosition(Width, i);
+        std::cout << "*";
     }
 
-    class Car
+    std::cout << "\033[0m"; // Reset console color
+}
+
+class Vehicle
+{
+    public:
+    int x, y;
+    Vehicle(int x, int y) : x(x), y(y) { }
+    virtual void move() = 0;
+    virtual char getSymbol() const = 0;
+};
+
+class Car : public Vehicle
+{
+public:
+    Car(int x, int y) : Vehicle(x, y) { }
+void move() override
+{
+    std::this_thread::sleep_for(std::chrono::milliseconds(Interval));
+    int direction = rand() % 2; // Cars move only along X axis
+
+    consoleLock.lock () ;
+    setCursorPosition(x, y);
+    std::cout << " ";
+
+    if (direction == 0)
     {
-        public int x, y;
+        x = std::min(x + Speed, Width - 1); // Move right
+    }
+    else
+    {
+        x = std::max(x - Speed, 0); // Move left
     }
 
-    class Motorcycle
-    {
-        public int x, y;
+    setCursorPosition(x, y);
+    std::cout << getSymbol();
+    consoleLock.unlock();
+}
+char getSymbol() const override {
+        return 'C';
     }
+};
+
+class Motorcycle : public Vehicle
+{
+public:
+    Motorcycle(int x, int y) : Vehicle(x, y) { }
+void move() override
+{
+    std::this_thread::sleep_for(std::chrono::milliseconds(100)); // Motorcycles move more frequently
+
+    consoleLock.lock () ;
+    setCursorPosition(x, y);
+    std::cout << " ";
+
+    int direction = rand() % 2; // Motorcycles move only along Y axis
+    if (direction == 0)
+    {
+        y = std::min(y + Speed, Height - 1); // Move down
+    }
+    else
+    {
+        y = std::max(y - Speed, 0); // Move up
+    }
+
+    setCursorPosition(x, y);
+    std::cout << getSymbol();
+    consoleLock.unlock();
+}
+char getSymbol() const override {
+        return 'M';
+    }
+};
+
+int main()
+{
+    srand(time(0));
+    std::cout << "\033[?25l"; // Hide cursor
+    drawBorder();
+
+    std::vector<Car> cars;
+    std::vector<Motorcycle> motorcycles;
+    std::vector<std::thread> threads;
+
+    // Initialize cars at random positions
+    for (int i = 0; i < NumberOfCars; ++i)
+    {
+        cars.emplace_back(rand() % Width, rand() % Height);
+        setCursorPosition(cars.back().x, cars.back().y);
+        std::cout << cars.back().getSymbol();
+    }
+
+    // Initialize motorcycles at random positions
+    for (int i = 0; i < NumberOfMoto; ++i)
+    {
+        motorcycles.emplace_back(rand() % Width, rand() % Height);
+        setCursorPosition(motorcycles.back().x, motorcycles.back().y);
+        std::cout << motorcycles.back().getSymbol();
+    }
+
+    // Start threads for cars
+    for (auto & car : cars) {
+    threads.emplace_back([&car]() {
+        while (true)
+        {
+            car.move();
+        }
+    });
+}
+
+// Start threads for motorcycles
+for (auto & motorcycle : motorcycles)
+{
+    threads.emplace_back([&motorcycle]() {
+        while (true)
+        {
+            motorcycle.move();
+        }
+    });
+    }
+
+    for (auto & thread : threads)
+{
+    thread.join();
+}
+
+std::cout << "\033[?25h"; // Show cursor
+
+return 0;
 }
